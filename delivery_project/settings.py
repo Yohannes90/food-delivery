1cad0711f1098fd4bc84507ad4ada3bc1b35167c
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-14 19:11:42 +0300

    updating project to add payment integration with yene pay

--- delivery_project/settings.py
+++ delivery_project/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'customer',
     'restaurant',
+    'yenepay',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- delivery_project/urls.py
+++ delivery_project/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import Index, About, OrderView
+from customer.views import (
+    Index,
+    About,
+    OrderView,
+    OrderConfirmation,
+    OrderPayConfirmation
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
     path('order/', OrderView.as_view(), name='order'),
+    path('order-conf/<int:pk>', OrderConfirmation.as_view(), name='order-conf'),
+    path('payment-conf/<int:pk>', OrderPayConfirmation.as_view(), name='payment-conf'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
