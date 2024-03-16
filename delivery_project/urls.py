08eb2c320ccf9ff92c2cdfac4e841466b27708b4
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-16 21:26:55 +0300

    updating project urls

--- delivery_project/urls.py
+++ delivery_project/urls.py
@@ -3,9 +3,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from customer.views import (
+    Menu,
     Index,
     About,
     OrderView,
+    MenuSearch,
     OrderConfirmation,
     OrderPayConfirmation
     )
@@ -16,6 +18,8 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
+    path('menu/', Menu.as_view(), name='menu'),
+    path('menu/search/', MenuSearch.as_view(), name='menu-search'),
     path('order/', OrderView.as_view(), name='order'),
     path('order-conf/<int:pk>', OrderConfirmation.as_view(), name='order-conf'),
     path('payment-conf/<int:pk>', OrderPayConfirmation.as_view(), name='payment-conf'),
