7f9c38ddc24dffb9fcf1622a37270fbeae502453
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-16 21:25:57 +0300

    updating restaurant app

--- restaurant/views.py
+++ restaurant/views.py
@@ -10,16 +10,43 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
         orders = Order.objects.filter(
             created_at__year=today.year, created_at__month=today.month, created_at__day=today.day)
 
+        unshipped_orders = []
         total_revenue = 0
         for order in orders:
             total_revenue += order.price
+            if not order.is_shipped:
+                unshipped_orders.append(order)
 
         context = {
-            'orders': orders,
+            'orders': unshipped_orders,
             'total_revenue': total_revenue,
             'total_orders': len(orders)
         }
         return render(request, 'restaurant/dashboard.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+
+
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = Order.objects.get(pk=pk)
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        order = Order.objects.get(pk=pk)
+        order.is_shipped = True
+        order.save()
+
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
 
     def test_func(self):
         return self.request.user.groups.filter(name='Staff').exists()
